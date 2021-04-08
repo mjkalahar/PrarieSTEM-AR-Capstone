a7269c4ce48df12ddf90a0472bb42bbf5aa38e2a
Matthew Kalahar <mjkalahar@gmail.com>
2021-04-07 19:38:52 -0500

    Add PiCameraDisplayAR, modified version of PiCameraDisplay to produce AR scene instead of normal image, edit Main scene to add AR after selecting feed from dropdown, Add Rocks and Boulders Assets, Add Texture Asset

---
+++ unityIntegration/Assets/PiCameraDisplayAR.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
+using NetMQ;
+using System.IO;
+using UnityEngine;
+using NetMQ.Sockets;
+using System.Diagnostics;
+using UnityEngine.UI;
+
+public class PiCameraDisplayAR : MonoBehaviour
+{
+    // The hardcoded (for now) pi IP and port
+    private int port;
+    private int offset = 10000;
+    private string serverIP = "tcp://ec2-54-164-70-144.compute-1.amazonaws.com:";
+    public Text FPSText;
+    private int LIMIT = 1000;
+    public Text AvgFPS;
+    private float lastTime;
+    private List<float> FPSList;
+
+    Texture2D camTexture;
+
+    RawImage screenDisplay;
+    public GameObject arToolkit;
+    public GameObject sceneRoot;
+
+    Canvas canvas;
+
+    // This is an object that spins up a thread to manage the socket connection
+    //and then hands off the data to a "messageDelegate" that handles the data
+    //in this instance the "messageDelegate" is the HandleMessage function
+    private NetMqListener _netMqListener;
+
+    // Script Start
+    private void Start()
+    {
+        lastTime = Time.unscaledTime;
+        port = DropDownMenu.port;
+        UnityEngine.Debug.Log(port);
+
+        FPSList = new List<float>();
+
+        //initialize cam texture and get the raw image
+        this.camTexture = new Texture2D(2, 2);
+        this.screenDisplay = GetComponent<RawImage>();
+        this.canvas = GetComponent<Canvas>();
+
+
+        // Create and start listener object
+        _netMqListener = new NetMqListener(HandleMessage, serverIP + (port + offset));
+        _netMqListener.Start();
+        arToolkit.GetComponent<ARController>().StartAR();
+        sceneRoot.GetComponent<AROrigin>().enabled = true;
+    }
+
+    // This function handles the message
+    // This is where we should put logic to display the image
+    private void HandleMessage(byte[] message)
+    {
+
+        this.camTexture.LoadImage(message);
+        byte[] jpeg = ImageConversion.EncodeToJPG(this.camTexture);
+        var dirPath = Application.dataPath + "/PiImages/";
+        if (!Directory.Exists(dirPath))
+        {
+            Directory.CreateDirectory(dirPath);
+        }
+        File.WriteAllBytes(dirPath + "Image" + ".jpeg", jpeg);
+        //4screenDisplay.texture = camTexture;
+
+        float diffTime = Time.unscaledTime - lastTime;
+        float fps = -1f;
+        if (diffTime > 0)
+        {
+            fps = 1f / diffTime;
+            FPSText.text = String.Format("FPS: {0:0.00}", fps);
+            lastTime = Time.unscaledTime;
+
+            if (fps >= 0)
+            {
+                if (FPSList.Count >= LIMIT)
+                {
+                    FPSList.RemoveAt(0);
+                }
+
+                FPSList.Add(fps);
+
+                float total = 0;
+
+                FPSList.ForEach(delegate (float value)
+                {
+                    total += value;
+                });
+                float avg = total / FPSList.Count;
+
+                AvgFPS.text = String.Format("Average FPS: {0:0.00}", avg);
+            }
+        }
+
+        Canvas.ForceUpdateCanvases();
+    }
+
+    // When our scripts update function is called, we update the listener
+    private void Update()
+    {
+        _netMqListener.Update();
+    }
+
+    // Stop the listener on program halt
+    private void OnDestroy()
+    {
+        _netMqListener.Stop();
+    }
+}
